3f765d22d8d4f7be716b9127de25eb554317e383
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-04-24 12:17:11 +0200

    video fixing

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Video = styled.video`
   display: block;
-  width: 100vw;
+  width: 100%;
   max-height: 56.25vw;
   -webkit-appearance: none;
   -moz-appearance: none;
