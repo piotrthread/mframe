78bdfb115504e6dd8cb35b04eccb3eba09145837
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-04-24 10:18:14 +0200

    Added Spacer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
 
 import Heading from "./components/Heading/Heading";
 import Menu from "./components/Menu/Menu";
@@ -8,11 +9,16 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Container from "./components/Container/Container";
 import Video from "./components/Video/Video";
 
+const Spacer = styled.div`
+  height: 70px;
+`;
+
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Menu />
+      <Spacer />
       {/* <Hero /> */}
       <Video controls autoPlay={true} muted width="100vw" height="auto">
         <source src="http://adsystem.pl/video/MFRAME.mov" type="video/mp4" />
