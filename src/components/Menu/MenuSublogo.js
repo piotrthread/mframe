55a21ae5f9e88f9dbca7f6806762d1448ef46693
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-05-29 12:54:35 +0200

    fixed sublogo

--- src/components/Menu/MenuSublogo.js
+++ src/components/Menu/MenuSublogo.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 const MenuSublogo = styled.span`
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 200;
+  @media (max-width: 500px) {
+    display:none;
+  }
 `;
 
 export default MenuSublogo;
