a8341f5bf1af2db68b348dcc49a012f958ff28af
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-06-04 13:43:23 +0200

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 import FooterWrapper from "./FooterWrapper";
+
+const MenuLogo = styled.span`
+  font-size: 200px;
+  font-weight: 900;
+  color: white;
+  position: absolute;
+  top: -40px;
+  right: 25px;
+`;
+const Paragraph = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  color: white;
+  margin-top: 140px;
+`;
+const Adress = styled.p`
+  color: white;
+  font-weight: 300;
+  font-size: 18px;
+  margin-top: 25px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top:25px;
+`;
+const Icon = styled.img`
+  width: 45px;
+  margin-right: 20px;
+`;
 
 const Footer = () => {
   return (
     <FooterWrapper>
-
+      <MenuLogo>a.</MenuLogo>
+      <Paragraph>Adsystem Sp. z o.o.</Paragraph>
+      <Adress>
+        Bielany Wrocławskie,
+        <br />
+        ul. Atramentowa 11
+        <br />
+        55-040 Kobierzyce
+        <br />
+        <br />
+        tel. +48 672 672 000
+        <br />
+        <br />
+        Dział handlowy: <br />
+        sales@adsystem.pl
+        <br />
+        <br />
+        Biuro: <br />
+        office@adsystem.pl
+      </Adress>
+      <SocialContainer>
+        <Icon src="./images/facebook.png" />
+        <Icon src="./images/youtube.png" />
+        <Icon src="./images/linkedin.png" />
+        <Icon src="./images/instagram.png" />
+      </SocialContainer>
     </FooterWrapper>
   );
 };
--- src/components/Footer/FooterWrapper.js
+++ src/components/Footer/FooterWrapper.js
@@ -2,15 +2,15 @@ import styled from "styled-components";
 
 const FooterWrapper = styled.div`
   background-color: black;
-  color: rgba(255, 255, 255, 0.8);
   font-weight: 300px;
   font-size: 13px;
-  height: 350px;
+  min-height: 90vh;
   margin-top: 150px;
   display: flex;
-  justify-content: center;
   flex-direction: column;
-  padding-left: 45px;
+  justify-content: flex-start;
+  padding: 50px;
+  position: relative;
 `;
 
 export default FooterWrapper;
