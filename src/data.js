6e9d5f46bd2378ec028de157309bd9d29ece8861
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-06-10 12:53:14 +0200

    added accesories image

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const modals = {
       "oświetlenie LED, półka, panele na zawieszki, uchwyty TV, kantorek,łuki",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
-    images: ["./images/placeholder1x1.jpg"],
+    images: ["./images/accesories.jpg"],
   },
   wyposazenie: {
     name: "Wyposażenie",
