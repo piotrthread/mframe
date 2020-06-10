0ca928ae3f0e4f7987bfd94614a7a8bc130230df
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-06-10 11:43:29 +0200

    added wyposazenie picture

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const modals = {
       "logotypy ze styroduru, logotypy podświetlane, uchwyty na katalogi,ekspozycja produktów klienta, meble, lady",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
-    images: ["./images/placeholder1x1.jpg"],
+    images: ["./images/additional_stuff.jpg"],
   },
   kasetony: {
     name: "Kasetony",
