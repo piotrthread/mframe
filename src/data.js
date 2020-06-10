b5451d8370deaf2947deaf56ecbeabf79e472871
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-06-10 10:01:03 +0200

    added floor picture

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 export const modals = {
   akcesoria: {
     name: "Akcesoria",
-    tags:"oświetlenie LED, półka, panele na zawieszki, uchwyty TV, kantorek,łuki",
+    tags:
+      "oświetlenie LED, półka, panele na zawieszki, uchwyty TV, kantorek,łuki",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
     images: ["./images/placeholder1x1.jpg"],
@@ -9,7 +10,8 @@ export const modals = {
   wyposazenie: {
     name: "Wyposażenie",
     subname: "stoiska",
-    tags:"logotypy ze styroduru, logotypy podświetlane, uchwyty na katalogi,ekspozycja produktów klienta, meble, lady",
+    tags:
+      "logotypy ze styroduru, logotypy podświetlane, uchwyty na katalogi,ekspozycja produktów klienta, meble, lady",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
     images: ["./images/placeholder1x1.jpg"],
@@ -17,7 +19,7 @@ export const modals = {
   kasetony: {
     name: "Kasetony",
     subname: "LED",
-    tags:"kaseton LMD, dynamic Lightbox, led skin",
+    tags: "kaseton LMD, dynamic Lightbox, led skin",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
     images: ["./images/placeholder1x1.jpg"],
@@ -25,15 +27,15 @@ export const modals = {
   podloga: {
     name: "Podłoga",
     subname: "systemowa",
-    tags:"panele jednokolorowe, drukowane, wykładzina",
+    tags: "panele jednokolorowe, drukowane, wykładzina",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
-    images: ["./images/placeholder1x1.jpg"],
+    images: ["./images/floor.jpg"],
   },
   podwieszane: {
     name: "Systemy",
     subname: "podwieszane",
-    tags:"vario, kaseton, quadro",
+    tags: "vario, kaseton, quadro",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
     images: ["./images/placeholder1x1.jpg"],
@@ -41,7 +43,8 @@ export const modals = {
   uslugi: {
     name: "Usługi",
     subname: "dodatkowe",
-    tags:"logistyka, montaż, support, magazynowanie, demontaż, stoisko 'pod klucz'",
+    tags:
+      "logistyka, montaż, support, magazynowanie, demontaż, stoisko 'pod klucz'",
     info:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in nibh turpis. Maecenas sit amet mattis erat, eu ultrices nisl. Nulla ac arcu libero. Nunc nec aliquet nibh. Sed id iaculis ante. Aenean sed nisi non neque ultrices luctus. Praesent porttitor dolor vitae dignissim dictum. Nullam sem elit, interdum non consectetur rutrum, facilisis et magna. Cras vel eleifend risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis dignissim nisi. Suspendisse sagittis tellus magna, in vulputate mi molestie et.",
     images: ["./images/placeholder1x1.jpg"],
