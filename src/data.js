88a1c692dfe9008e9622ec0f48fcc624c1e1869b
piotrthread <37698474+piotrthread@users.noreply.github.com>
2020-06-15 15:44:58 +0200

    podwiesznae

--- src/data.js
+++ src/data.js
@@ -38,7 +38,7 @@ export const modals = {
     tags: "vario, kaseton, quadro",
     info:
       "Donut icing jujubes marshmallow. Bear claw tart cookie. Bear claw pastry brownie marzipan lemon drops. Icing jelly beans topping brownie lollipop jelly beans tart carrot cake. Halvah jelly-o ice cream sweet roll icing chocolate bar cake. Cheesecake sesame snaps brownie ice cream cheesecake apple pie. Icing apple pie tiramisu lollipop danish topping dessert. Bear claw sesame snaps toffee chocolate cake. Chocolate bar oat cake cupcake. Cupcake pastry oat cake carrot cake. Marzipan soufflé dragée jelly tootsie roll cookie. Cotton candy jujubes sweet roll toffee. Pastry lollipop wafer. Sesame snaps wafer tiramisu lollipop. Cupcake donut sweet roll. Topping muffin gummi bears muffin jelly beans. Gingerbread caramels apple pie sweet biscuit candy pastry. Marzipan liquorice cotton candy wafer tootsie roll. Pastry cheesecake chupa chups tart jujubes sugar plum. Oat cake ice cream chocolate cake candy canes. Gummi bears powder jelly carrot cake jelly beans. Wafer biscuit dessert dragée jelly beans. Marzipan lemon drops lemon drops gingerbread bear claw sweet roll marzipan toffee. Dessert gummies caramels.",
-    images: ["./images/placeholder1x1.jpg"],
+    images: ["./images/hanging.jpg"],
   },
   uslugi: {
     name: "Usługi",
